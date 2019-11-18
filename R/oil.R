74573b7f8d2930dbffaa9a48a5e7bd44b6521427
martinbaumgaertner <40661076+martinbaumgaertner@users.noreply.github.com>
2019-11-18 14:10:13 +0100

    changed data help

--- R/oil.R
+++ R/oil.R
@@ -11,6 +11,12 @@
 #'
 #' @keywords datasets
 #'
+#' @examples
+#' data(oil)
+#' ydata<-data[,3:5]
+#' z<-data[,6]
+#' years<-data[,1:2]
+#'
 #' @references Olea, Stock and Watson (2018)
 #' (\href{https://uc4384f22718973d3d912d614f2e.dl.dropboxusercontent.com/cd/0/inline2/Aslwi_FA9z6v37dfxMeUP3mEaP-a4ZA8RFrXd5h_EymM1lqGy-UUwGJB3TxqRxluMhE9ebnvHEMZktfcUmJ1v4mGSxeS_ZEDJBCrjGvyopAnP44ClPLJ6kmM36F557kHnqKQjn___0XTNvPaWvXnkEC0Btt3MpVhn41q0ddL4qtZ1g9l2ooeVweHH9X5xCAaG3R28lpzivQzj8AZRVSPHs98yseh0L9I10-G0frVqteUkjuQJQNAmFlEPpqegmDhpPo_vaMScx1w1ivQ--mxoZli2uvBI1Pnen-nGIyD3qT_oT-0r3K6bjwk1vI71fgMtGw6ZhWWFuWv48Oq-vg7lN1fKgPEyvcJMcdIISnra0z-1w/file})
 #'
