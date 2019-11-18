6a36097201c264d9de3a96da61bc8fd333b3eb23
martinbaumgaertner <40661076+martinbaumgaertner@users.noreply.github.com>
2019-11-18 15:04:51 +0100

    removed unused variables

--- R/SVARIV.R
+++ R/SVARIV.R
@@ -2,20 +2,14 @@
 #'
 #' Implements standard and weak-IV robust SVAR-IV inference.
 #'
-#' @param p Number of lags in the VAR model
-#' @param confidence   Value for the standard and weak-IV robust confidence set
 #' @param ydata        Endogenous variables from the VAR model
 #' @param z            External instrumental variable
+#' @param p Number of lags in the VAR model
+#' @param confidence   Value for the standard and weak-IV robust confidence set
 #' @param NWlags       Newey-West lags
 #' @param norm         Variable used for normalization
 #' @param scale        Scale of the shock
 #' @param horizons     Number of horizons for the Impulse Response Functions
-#' @param savdir       Directory where the figures generated will be saved
-#' @param columnnames  Vector with the names for the endogenous variables, in the same order as ydata
-#' @param IRFselect    Indices for the variables that the user wants separate IRF plots for
-#' @param cumselect    Indices for the variables that the user wants cumulative IRF plots for
-#' @param time         Time unit for the dataset e.g. year, month, etc.
-#' @param dataset_name The name of the dataset used for generating the figures
 #'
 #' @return      PLugin:       Structure containing standard plug-in inference
 #' @return       InferenceMSW: Structure containing the MSW weak-iv robust confidence interval
@@ -24,9 +18,9 @@
 #'
 #' @export
 
-SVARIV<-function(p,confidence, ydata, z, NWlags, norm, scale, horizons, savdir, columnnames, IRFselect,
-                 cumselect, time, dataset_name){
-  SVARIV_Check(p,confidence, ydata, z, NWlags, norm, scale, horizons, savdir, columnnames, IRFselect, cumselect, time, dataset_name)
+SVARIV<-function(ydata, z, p, confidence, NWlags, norm, scale, horizons, columnnames,
+                 time, dataset_name){
+  SVARIV_Check(p,confidence, ydata, z, NWlags, norm, scale, horizons, columnnames, IRFselect, cumselect, time, dataset_name)
 
   SVARinp<-list(ydata=ydata,
                 Z=z,
